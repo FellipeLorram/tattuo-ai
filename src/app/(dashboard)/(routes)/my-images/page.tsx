12972ba5374041aa1981d4d4e730394000af4e98
Fellipe Lorram <fellipelorram@gmail.com>
2023-07-27 18:28:39 -0300

    Fix .gitignore to ignore .env file

--- src/app/(dashboard)/(routes)/my-images/page.tsx
+++
@@ -1,9 +0,0 @@
-import { EmptyBoxAnimation } from '@/components/layout/emptyBoxAnimation';
-
-export default function page() {
-  return (
-    <div className='flex-1 flex items-center justify-center'>
-      <EmptyBoxAnimation/>
-    </div>
-  )
-}
--- src/lib/generateImageForm.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const generateImageSchema = z.object({
-	prompt: z.string().min(3, { message: 'No minimo 3 caracteres' }),
-	quantity: z.string().min(1, { message: 'No minimo 1' }),
-	resolution: z.string(),
-});
-
-export type GenerateImageFormType = z.infer<typeof generateImageSchema>;
